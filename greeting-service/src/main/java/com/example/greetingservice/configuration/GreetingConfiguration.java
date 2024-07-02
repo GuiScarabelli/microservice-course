076c45688130b745f95a3ae1af20b3eea60a5ebe
GuiScarabelli <guilherme.quadros@sptech.school>
2024-07-02 11:40:54 -0300

    Adding configuration files

--- greeting-service/src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
@@ -2,9 +2,11 @@ package com.example.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope //
 @ConfigurationProperties("greeting-service") //Faz a relação entre o arquivo de conexão e a classe java
 public class GreetingConfiguration {
   private String greeting;
