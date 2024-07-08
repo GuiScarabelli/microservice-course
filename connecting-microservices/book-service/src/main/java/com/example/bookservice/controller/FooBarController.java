2c5ab2dec142f12b9ff41d3c08e1b7c261df16ed
GuiScarabelli <guilherme.quadros@sptech.school>
2024-07-08 15:47:13 -0300

    Adding and Testing resilience4j features

--- connecting-microservices/book-service/src/main/java/com/example/bookservice/controller/FooBarController.java
+++ connecting-microservices/book-service/src/main/java/com/example/bookservice/controller/FooBarController.java
@@ -1,5 +1,8 @@
 package com.example.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,23 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping
+@RequestMapping("/books")
 public class FooBarController {
 
   private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
   @GetMapping("/foo-bar")
+
   // Annotation to retry the request, can be customized in application.yml
-  @Retry(name = "default", fallbackMethod = "fallbackMethod")
+  // @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Determine how many times it's going to request
+  // @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
+  // @RateLimiter(name = "foo-bar") // Determine the number of requests
+
+  @Bulkhead(name = "default") // determmine how many concurrent-calls
   public String fooBar(){
     logger.info("Request to foo-bar is received!");
-    var response = new RestTemplate()
-            .getForEntity("http://localhost:8080/foo-bar", String.class);
-    return response.getBody();
+    //var response = new RestTemplate()
+    //        .getForEntity("http://localhost:8080/foo-bar", String.class);
+    //return response.getBody();
+    return "foo-bar";
   }
 
   public String fallbackMethod(Exception ex){
     return "fallback method!!!";
   }
-
 }
