81ce8a5a4ea8f5309b3b0977ee9ee318489fa7a9
GuiScarabelli <guilherme.quadros@sptech.school>
2024-07-10 12:44:54 -0300

    Adding and configuring the Swagger OpenApi on Cambio Service

--- connecting-microservices/cambio-service/src/main/java/com/example/cambioservice/controller/ExchangeController.java
+++ connecting-microservices/cambio-service/src/main/java/com/example/cambioservice/controller/ExchangeController.java
@@ -5,12 +5,15 @@ import com.example.cambioservice.mapper.ExchangeListing;
 import com.example.cambioservice.mapper.ExchangeMapper;
 import com.example.cambioservice.repository.ExchangeRepository;
 import com.example.cambioservice.service.ExchangeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
+@Tag(name = "Exchange endpoint")
 @RestController
 @RequestMapping("/exchanges")
 public class ExchangeController {
@@ -21,6 +24,7 @@ public class ExchangeController {
   @Autowired
   private Environment environment;
 
+  @Operation(summary = "Get the exchange from the currency")
   @GetMapping("/{amount}/{from}/{to}")
   private ResponseEntity<ExchangeListing> getExchange(@PathVariable Double amount,
                                                       @PathVariable String from,
